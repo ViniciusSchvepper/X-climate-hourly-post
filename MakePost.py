7255fc0e3568bbe9bdc108e77e9bcf125be190d7
ViniciusSchvepper <vynycyus_hecate@hotmail.com>
2024-06-25 16:19:32 -0300

    Commit de arquivo

--- MakePost.py
+++ MakePost.py
@@ -1,15 +1,17 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from GetWeatherData import get_weather_data
 import time
 
 def MakePost(driver):
+    string_result = get_weather_data()
     time.sleep
     post_input_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div'
     post_input = WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.XPATH, post_input_xpath))
     )
-    post_input.send_keys('Teste brabo')
+    post_input.send_keys(string_result)
 
     send_post_button_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/button'
     send_post = WebDriverWait(driver, 10).until(
